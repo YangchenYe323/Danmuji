7b58576a7e8d59f2b051ddf75917968f47cf77bd
Ye Yangchen <yye02@email.wm.edu>
2022-05-23 20:54:37 -0400

    add lint config

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,45 @@
+/* eslint-env node */
+
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaFeatures": {
+			"jsx": true
+		},
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"react",
+		"@typescript-eslint"
+	],
+	"rules": {
+		// suppress errors for missing 'import React' in files
+		"react/react-in-jsx-scope": "off",
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
